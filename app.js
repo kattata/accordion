d3a5f91d83ab55b2845f6a48852fc617941aa108
Kasia Laniecka <kl@parkshare.dk>
2022-03-21 19:04:03 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const data = [
+  {
+    title: "One",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "Two",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.",
+  },
+  {
+    title: "Three",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    title: "Four",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.",
+  },
+  {
+    title: "Five",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.",
+  },
+];
+
+const accordion = document.querySelector(".accordion");
+
+const displayData = () => {
+  let html = "";
+  data.map((item) => {
+    html += `
+          <div class="accordion-item">
+            <div class="flex">
+                <h2 class="header">${item.title}</h2>
+                <span>
+                <img src="assets/plus-blue.png" class="btn"/>
+                </span>
+            </div>
+            <p class="body">${item.body}</p>
+          </div>
+        `;
+  });
+  accordion.innerHTML = html;
+};
+
+displayData();
+
+const btns = document.querySelectorAll(".btn");
+const items = document.querySelectorAll(".accordion-item");
+const containers = document.getElementsByClassName(".container");
+
+const toggle = (e) => {
+  items.forEach((btn) => {
+    let button = e.target;
+    let buttonSpan = e.target.parentElement;
+    let item = e.target.parentElement.parentElement.parentElement;
+    if (buttonSpan.classList.contains("active")) {
+      buttonSpan.classList.remove("active");
+      button.src = "assets/plus-blue.png";
+      item.style.color = "white";
+    } else {
+      buttonSpan.classList.add("active");
+      button.src = "assets/minus-white.png";
+      item.style.color = "black";
+    }
+
+    // let item = e.target.parentElement.parentElement.parentElement;
+    item.classList.toggle("active");
+  });
+
+  let body =
+    e.target.parentElement.parentElement.parentElement.lastElementChild;
+  body.classList.toggle("active");
+  if (body.classList.contains("active")) {
+    let bodyHeight = `${body.scrollHeight + 20}px`;
+    body.style.maxHeight = bodyHeight;
+  } else {
+    body.style.maxHeight = "0px";
+  }
+};
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => toggle(e));
+});

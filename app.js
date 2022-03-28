1e4c9d9e9275e1be7530804286b4d68fc5dd4502
Kasia Laniecka <kl@parkshare.dk>
2022-03-28 21:12:16 +0200

    width

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const data = [
   },
   {
     title: "Engelsk",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
   },
   {
     title: "Tysk",
